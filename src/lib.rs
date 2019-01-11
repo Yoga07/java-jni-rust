0324d1c36509c24f68c7551221f26389c7ef2b98
Yoga07 <yogeshwar_1997@hotmail.com>
2019-01-11 05:22:19 -0800

    Error commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,21 @@
 extern crate jni;
 
-use jni::JNIEnv;
 use jni::objects::{JClass, JObject, JString, JValue};
-use jni::sys::{jint, jstring, jobject, jdouble};
+use jni::sys::{_jobject, jdouble, jint, jobject, jstring};
+use jni::JNIEnv;
+use jni::JavaVM;
+use std::marker::Send;
+use std::sync::mpsc::channel;
 use std::{thread, time};
 
+
 #[no_mangle]
-pub extern "system" fn Java_com_sample_jni_Library_printMsg(
-    _env: JNIEnv,
-    _class: JClass
-    ) {
+pub extern "system" fn Java_com_sample_jni_Library_printMsg(_env: JNIEnv, _class: JClass) {
     println!("[Rust]Hello World!");
 }
 
 #[no_mangle]
-pub extern "system" fn Java_com_sample_jni_Library_returnInt(
-    _env: JNIEnv,
-    _class: JClass
-    ) -> jint {
+pub extern "system" fn Java_com_sample_jni_Library_returnInt(_env: JNIEnv, _class: JClass) -> jint {
     let o: jint = 8;
     o
 }
@@ -25,8 +23,8 @@ pub extern "system" fn Java_com_sample_jni_Library_returnInt(
 #[no_mangle]
 pub extern "system" fn Java_com_sample_jni_Library_returnString(
     _env: JNIEnv,
-    _class: JClass
-    ) -> jstring {
+    _class: JClass,
+) -> jstring {
     let s = _env.new_string("[Java]hello").expect("error");
     s.into_inner()
 }
@@ -36,8 +34,8 @@ pub extern "system" fn Java_com_sample_jni_UserData_createUser(
     env: JNIEnv,
     _class: JClass,
     name: JString,
-    balance: jdouble
-    ) -> jobject {
+    balance: jdouble,
+) -> jobject {
     let user_data_class = env
         .find_class("Lcom/sample/jni/UserData;")
         .expect("Could not find class");
@@ -50,39 +48,51 @@ pub extern "system" fn Java_com_sample_jni_UserData_createUser(
         new_user_data,
         "name",
         "Ljava/lang/String;",
-        JValue::from(JObject::from(name)))
-        .expect("Could not set name field");
+        JValue::from(JObject::from(name)),
+    )
+    .expect("Could not set name field");
 
-    env.set_field(
-        new_user_data,
-        "balance",
-        "D",
-        JValue::from(balance))
+    env.set_field(new_user_data, "balance", "D", JValue::from(balance))
         .expect("Could not set balance field");
 
     new_user_data.into_inner()
 }
 
 #[no_mangle]
+
 pub extern "system" fn Java_com_sample_jni_UserData_printUserData(
-    _env: JNIEnv,
+    env: JNIEnv,
     _class: JClass,
     _obj1: JObject,
-    obj2: JObject
-    ) {
-        let handle = thread::spawn(|| {
+    obj2: JObject,
+) {
+    let JVM = env.get_java_vm().unwrap();
+
+    let mut packet: Option<JObject> = Some(obj2);
+
+    //THREAD 1 does Rust functions
+    let handle = thread::spawn(move || {
+        let env1 = JVM.attach_current_thread().unwrap();
         println!("[Rust]Rust is processing");
-        let sleeptime = time::Duration::from_secs(3);
-        thread::sleep(sleeptime);
+        let sleep_time = time::Duration::from_secs(10);
+        thread::sleep(sleep_time);
         println!("[Rust]Rust finishes processing");
-        });
-        let z = _env.new_string("Rust->Calls back to Java\n[Java]Java executes callbacks").expect("error creating java string");
+    });
+    //THREAD 2 calls back to Java
+    let handle2 = thread::spawn(move || {
+        let env2 = JVM.attach_current_thread().unwrap();
+
+        let y: &mut Option<JObject> = &mut packet;
+
+        let mut object = y.take().unwrap();
+
+        let z = env2
+            .new_string("Rust->Calls back to Java\n[Java]Java executes callbacks")
+            .expect("error creating java string");
+
         let q = JValue::Object(JObject::from(z));
-        _env.call_method(
-            obj2,
-            "call",
-            "(Ljava/lang/String;)V",
-            &[q])
+
+        env2.call_method(object, "call", "(Ljava/lang/String;)V", &[q])
             .expect("error in calling method from java");
-        handle.join().unwrap();
+    });
 }
